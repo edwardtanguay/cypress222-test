61f840c16cac3fa0a09486a19061595cb2a9dd12
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-09 12:01:53 +0100

    finished

--- cypress/e2e/react-site/mainpage.cy.js
+++ cypress/e2e/react-site/mainpage.cy.js
@@ -1,8 +1,25 @@
-it('loads page', () => {
-	cy.visit('http://127.0.0.1:5173');
+beforeEach(() => {
+	cy.visit('/');
 });
 
-it('header has correct text', () => {
-	cy.visit('http://127.0.0.1:5173');
+it('the page loads', () => {});
+
+it('the header text is correct', () => {
 	cy.get('h1').invoke('text').should('equal', 'The Info Site');
-})
+});
+
+context('message box', () => {
+	it('the message does not show when no text is in box', () => {
+		// cy.get('[data-cy="numberOfChars"]').as('numchars');
+		cy.get('[data-cy="numberOfChars"]').should('not.exist');
+	});
+
+	it('the message reports number of characters that are typed in the box', () => {
+		cy.get('input').type('this is a test');
+
+		// cy.get('[data-cy="numberOfChars"]').as('numchars');
+		cy.get('[data-cy="numberOfChars"]')
+			.invoke('text')
+			.should('equal', '14');
+	});
+});
